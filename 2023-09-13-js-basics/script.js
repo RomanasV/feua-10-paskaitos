1ebaab3653db44e46c77293b4c02d44fe62d757e
RomanasV <romanasvenckus@gmail.com>
2023-09-13 20:04:14 +0300

    js basics task

--- 2023-09-13-js-basics/script.js
+++ 2023-09-13-js-basics/script.js
@@ -4,6 +4,36 @@
 // 3. Lygybės ženklas
 // 4. Kintamojo vertė
 
-var vardas = 'Steve' // String (tekstas) duomenų tipas
+var vardas = 'John' // String (tekstas) duomenų tipas
+let pavarde = "Doe" // String (tekstas) duomenų tipas
+let miestas = `Kaunas` // String (tekstas) duomenų tipas
+let amzius = 30 // Number (skaičius) duomenų tipas
+let yraStudentas = false // Boolean (true/false) duomenų tipas
+const asmensKodas = 345487845454 // Number (skaičius) duomenų tipas
 
 console.log(vardas)
+console.log(pavarde)
+console.log(miestas)
+console.log(amzius)
+console.log(yraStudentas)
+console.log(asmensKodas)
+
+amzius = 31
+console.log(amzius)
+
+vardas = 'Steve'
+console.log(vardas)
+
+console.log(vardas, typeof vardas)
+console.log(pavarde, typeof pavarde)
+console.log(amzius, typeof amzius)
+console.log(yraStudentas, typeof yraStudentas)
+
+
+console.log('Hello, ' + vardas + ' ' + pavarde + '.')
+console.log("Hello, " + vardas + " " + pavarde + ".")
+console.log(`Hello, ${vardas} ${pavarde}.`)
+
+// John Doe (amžius 25 m.), asmens kodas: 37878787878, gyvenamasis miestas: Kaunas.
+
+console.log('John Doe (amžius 25 m.), asmens kodas: 37878787878, gyvenamasis miestas: Kaunas.')
