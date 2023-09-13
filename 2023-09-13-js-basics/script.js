b0c02ce35c231032e6b6a132ed467f772bbb12db
RomanasV <romanasvenckus@gmail.com>
2023-09-13 19:10:21 +0300

    script file created

---
+++ 2023-09-13-js-basics/script.js
@@ -0,0 +1,9 @@
+// Kintamojo sukūrimas:
+// 1. Iniciavimo žodelis (var, let, const)
+// 2. Kintamojo pavadinimas
+// 3. Lygybės ženklas
+// 4. Kintamojo vertė
+
+var vardas = 'Steve' // String (tekstas) duomenų tipas
+
+console.log(vardas)
