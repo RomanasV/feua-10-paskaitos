d6a5ef6690803ed3351146be04aa72fca98c3f92
RomanasV <romanasvenckus@gmail.com>
2023-09-19 19:38:32 +0300

    tasks done

--- 2023-09-13-js-basics/script.js
+++ 2023-09-13-js-basics/script.js
@@ -374,6 +374,7 @@ if (personAge >= 18) {
 }
 
 console.groupEnd()
+console.groupCollapsed('Tasks 1')
 
 // SLAPTAŽODIS:
 // 1. Jeigu slaptažodis yra trumpesnis už 16 simbolių, tai parašyti jog „Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo."
@@ -511,25 +512,348 @@ if (isNaN(age)) {
   console.log('Nurodytas amžius yra per didelis, jis negali būti didesnis už 120 metų.')
 }
 
+console.groupEnd()
+
+// UŽDUOTIS: sukurti galvosūkį su keletu klausimu
+// PIRMAS LYGIS:
+// 1. Norint patekti į kitą lygį, reikia atsakyti bent į vieną klausimą iš dviejų:
+// 2. Jeigu atsakytas tik vienas klausimas, tai papildomai reikia parašyti kuris klausimas buvo neteisingas.
+
+// 1. Patekai i kita lygi: abu atsakymai teisingi.
+// 2. Patekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.
+// 3. Patekai i kita lygi: antras atsakymas buvo neteisingas, o pirmas buvo teisingas.
+// 4. Nepatekai i kita lygi: abu atsakymai buvo neteisingi.
+
 // Kokia yra arčiausia Žemės esanti žvaigždė?
 let correctAnswer11 = 'sun'
 
 // Kiek yra 123 + 456
 let correctAnswer12 = 579
 
-let playerAnswer11 = 'sun'
-let playerAnswer12 = 45646
+let playerAnswer11 = 'moon'
+let playerAnswer12 = 579
 
-console.log(correctAnswer11 === playerAnswer11)
-console.log(correctAnswer12 === playerAnswer12)
+// if (correctAnswer11 === playerAnswer11 && correctAnswer12 === playerAnswer12) {
+//   console.log('Patekai i kita lygi: abu atsakymai teisingi.')
+// } else if (correctAnswer11 !== playerAnswer11 && correctAnswer12 === playerAnswer12) {
+//   console.log('Patekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
+// } else if (correctAnswer11 === playerAnswer11 && correctAnswer12 !== playerAnswer12) {
+//   console.log('Patekai i kita lygi: antras atsakymas buvo neteisingas, o pirmas buvo teisingas.')
+// } else {
+//   console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+// }
 
-console.log(correctAnswer11 === playerAnswer11 && correctAnswer12 === playerAnswer12)
+if (correctAnswer11 === playerAnswer11 && correctAnswer12 === playerAnswer12) {
+  console.log('Patekai i kita lygi: abu atsakymai teisingi.')
+} else if (correctAnswer12 === playerAnswer12) {
+  console.log('Patekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
+} else if (correctAnswer11 === playerAnswer11) {
+  console.log('Patekai i kita lygi: antras atsakymas buvo neteisingas, o pirmas buvo teisingas.')
+} else {
+  console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+}
+
+// ANTRAS LYGIS:
+// 1. Norint patekti į kitą lygį, reikia atsakyti į abu klausimus iš dviejų:
+// 2. Jeigu atsakytas tik vienas klausimas, tai papildomai reikia parašyti kuris atsakymas buvo neteisingas.
+
+let correctAnswer21 = 1
+let correctAnswer22 = 1
+
+let playerAnswer21 = 1
+let playerAnswer22 = 1
+
+if (correctAnswer21 === playerAnswer21 && correctAnswer22 === playerAnswer22) {
+  console.log('Patekai i kita lygi: abu atsakymai teisingi.')
+} else if (correctAnswer22 === playerAnswer22) {
+  console.log('Nepatekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
+} else if (correctAnswer21 === playerAnswer21) {
+  console.log('Nepatekai i kita lygi: antras atsakymas buvo neteisingas, o pirmas buvo teisingas.')
+} else {
+  console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+}
+
+// TREČIAS LYGIS: 
+// 1. Trys klausimai ir į kitą lygį pereinama, jeigu atsakomi bent du klausimai.
+// 2. Jeigu atsakomi du klausimai, turi būti parodoma, kuris klausimas buvo neatsakytas.
+// 3. Jeigu atsakomi mažiau nei du klausimai, tai turi parodyti, kuris klausimas buvo atsakytas.
+
+// 1 1 1 +
+// 1 1 0 +
+// 1 0 1 +
+// 0 1 1 +
+// 1 0 0 +
+// 0 1 0 +
+// 0 0 1 +
+// 0 0 0 +
+
+// let correctAnswer31 = 1
+// let correctAnswer32 = 1
+// let correctAnswer33 = 1
+
+// let playerAnswer31 = 0
+// let playerAnswer32 = 0
+// let playerAnswer33 = 0
+
+// if (correctAnswer31 === playerAnswer31 && correctAnswer32 === playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: visi atsakymai teisingi.')
+// } else if (correctAnswer31 === playerAnswer31 && correctAnswer32 === playerAnswer32 && correctAnswer33 !== playerAnswer33) {
+//   console.log('Patekai i kita lygi: tačiau trečias atsakymas buvo neteisingas.')
+// } else if (correctAnswer31 === playerAnswer31 && correctAnswer32 !== playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: tačiau antras atsakymas buvo neteisingas.')
+// } else if (correctAnswer31 !== playerAnswer31 && correctAnswer32 === playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: tačiau pirmas atsakymas buvo neteisingas.')
+// } else if (correctAnswer31 === playerAnswer31 && correctAnswer32 !== playerAnswer32 && correctAnswer33 !== playerAnswer33) {
+//   console.log('Nepatekai i kita lygi: tačiau pirmas atsakymas buvo teisingas.')
+// } else if (correctAnswer31 !== playerAnswer31 && correctAnswer32 === playerAnswer32 && correctAnswer33 !== playerAnswer33) {
+//   console.log('Nepatekai i kita lygi: tačiau antras atsakymas buvo teisingas.')
+// } else if (correctAnswer31 !== playerAnswer31 && correctAnswer32 !== playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Nepatekai i kita lygi: tačiau trečias atsakymas buvo teisingas.')
+// } else {
+//   console.log('Nepatekai i kita lygi: visi atsakymai buvo neteisingi.')
+// }
+
+// if (correctAnswer31 === playerAnswer31 && correctAnswer32 === playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: visi atsakymai teisingi.')
+// } else if (correctAnswer31 === playerAnswer31 && correctAnswer32 === playerAnswer32) {
+//   console.log('Patekai i kita lygi: tačiau trečias atsakymas buvo neteisingas.')
+// } else if (correctAnswer31 === playerAnswer31 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: tačiau antras atsakymas buvo neteisingas.')
+// } else if (correctAnswer32 === playerAnswer32 && correctAnswer33 === playerAnswer33) {
+//   console.log('Patekai i kita lygi: tačiau pirmas atsakymas buvo neteisingas.')
+// } else if (correctAnswer31 === playerAnswer31) {
+//   console.log('Nepatekai i kita lygi: tačiau pirmas atsakymas buvo teisingas.')
+// } else if (correctAnswer32 === playerAnswer32) {
+//   console.log('Nepatekai i kita lygi: tačiau antras atsakymas buvo teisingas.')
+// } else if (correctAnswer33 === playerAnswer33) {
+//   console.log('Nepatekai i kita lygi: tačiau trečias atsakymas buvo teisingas.')
+// } else {
+//   console.log('Nepatekai i kita lygi: visi atsakymai buvo neteisingi.')
+// }
 
 
+let correctAnswer31 = 1
+let correctAnswer32 = 1
+let correctAnswer33 = 1
+
+let playerAnswer31 = 1
+let playerAnswer32 = 1
+let playerAnswer33 = 0
+
+let answer31 = correctAnswer31 === playerAnswer31
+let answer32 = correctAnswer32 === playerAnswer32
+let answer33 = correctAnswer33 === playerAnswer33
+
+let successMessage = 'Patekai i kita lygi: '
+let failMessage = 'Nepatekai i kita lygi: '
+
+let output = ''
+
+if (answer31 && answer32 && answer33) {
+  output = successMessage + 'visi atsakymai teisingi.'
+} else if (answer31 && answer32) {
+  output = successMessage + 'tačiau trečias atsakymas buvo neteisingas.'
+} else if (answer31 && answer33) {
+  output = successMessage + 'tačiau antras atsakymas buvo neteisingas.'
+} else if (answer32 && answer33) {
+  output = successMessage + 'tačiau pirmas atsakymas buvo neteisingas.'
+} else if (answer31) {
+  output = failMessage + 'tačiau pirmas atsakymas buvo teisingas.'
+} else if (answer32) {
+  output = failMessage + 'tačiau antras atsakymas buvo teisingas.'
+} else if (answer33) {
+  output = failMessage + 'tačiau trečias atsakymas buvo teisingas.'
+} else {
+  output = failMessage + 'visi atsakymai buvo neteisingi.'
+}
+
+console.log(output)
+
+
+
+// Pasisveikinimas
+// 1. Jeigu vartotojas prisijungęs (true/false), tai prie pasisveikinimo reikia prirašyti jo vardą, pvz. „Good Morning, Tom.".
+// 2. Jeigu vartotojas nėra prisijungęs, tai išvesti tik tekstą „Good Morning".
+// 3. Priklausomai nuo paros laiko, pasisveikinimas turėtų būti skirtingas:
+// 3.1. 5-12 val. „Good Morning"
+// 3.2. 13-18 val. „Good Afternoon"
+// 3.3. 19-4 val. „Good Evening"
+// 4. Jeigu vartotojas yra ir prisijungęs, ir šiandien yra jo gimtadienis, tai prie pasisveikinimo dar turi būti parašytas ir pasveikinimas, pvz.: „Good Morning, Tom and have a great birthday!"
+
+
+// let isLoggedIn = true
+// let userName = 'John'
+// let time = 18
+// let isBirthday = false
+
+// if (isLoggedIn === true) {
+  
+//   if (time >= 5 && time < 13) {
+
+//     if (isBirthday === true) {
+//       console.log(`Good Morning, ${userName} and have a great birthday!`)
+//     } else {
+//       console.log(`Good Morning, ${userName}.`)
+//     }
+
+//   } else if (time >= 13 && time < 19) {
+
+//     if (isBirthday === true) {
+//       console.log(`Good Afternoon, ${userName} and have a great birthday!`)
+//     } else {
+//       console.log(`Good Afternoon, ${userName}.`)
+//     }
+
+//   } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+
+//     if (isBirthday === true) {
+//       console.log(`Good Evening, ${userName} and have a great birthday!`)
+//     } else {
+//       console.log(`Good Evening, ${userName}.`)
+//     }
+
+//   } else {
+    
+//     if (isBirthday === true) {
+//       console.log(`Hello, ${userName} and have a great birthday!`)
+//     } else {
+//       console.log(`Hello, ${userName}.`)
+//     }
+
+//   }
+
+// } else {
+  
+//   if (time >= 5 && time < 13) {
+//     console.log(`Good Morning`)
+//   } else if (time >= 13 && time < 19) {
+//     console.log(`Good Afternoon`)
+//   } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+//     console.log(`Good Evening`)
+//   } else {
+//     console.log(`Hello`)
+//   }
+
+// }
+
+// if (time >= 5 && time < 13) {
+  
+//   if (isLoggedIn === true && isBirthday === true) {
+//     console.log(`Good Morning, ${userName} and have a great birthday!`)
+//   } else if (isLoggedIn === true) {
+//     console.log(`Good Morning, ${userName}`)
+//   } else {
+//     console.log(`Good Morning`)
+//   }
+
+// } else if (time >= 13 && time < 19) {
+
+//   if (isLoggedIn === true && isBirthday === true) {
+//     console.log(`Good Afternoon, ${userName} and have a great birthday!`)
+//   } else if (isLoggedIn === true) {
+//     console.log(`Good Afternoon, ${userName}`)
+//   } else {
+//     console.log(`Good Afternoon`)
+//   }
+
+// } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+
+//   if (isLoggedIn === true && isBirthday === true) {
+//     console.log(`Good Evening, ${userName} and have a great birthday!`)
+//   } else if (isLoggedIn === true) {
+//     console.log(`Good Evening, ${userName}`)
+//   } else {
+//     console.log(`Good Evening`)
+//   }
+
+// } else {
+
+//   if (isLoggedIn === true && isBirthday === true) {
+//     console.log(`Hello, ${userName} and have a great birthday!`)
+//   } else if (isLoggedIn === true) {
+//     console.log(`Hello, ${userName}`)
+//   } else {
+//     console.log(`Hello`)
+//   }
+
+// }
+
+
+
+
+
+
+
+
+
+// let isLoggedIn = true
+// let userName = 'Steve'
+// let time = 20
+// let isBirthday = false
+
+// let greetingText = ''
+// let nameText = ''
+// let birthdayText = ''
+
+// if (time >= 5 && time < 13) {
+//   greetingText = 'Good Morning'
+// } else if (time >= 13 && time < 19) {
+//   greetingText = 'Good Afternoon'
+// } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+//   greetingText = 'Good Evening'
+// } else {
+//   greetingText = 'Hello'
+// }
+
+// if (isLoggedIn && userName) {
+//   nameText = ', ' + userName
+// }
+
+// if (isBirthday && isLoggedIn) {
+//   birthdayText = ' and have a great birthday!'
+// }
+
+// let greetingOutput = greetingText + nameText + birthdayText
+
+// console.log(greetingOutput)
 
 
 
 let isLoggedIn = true
-let userName = 'John'
-let time = 5
+let userName = 'Steve'
+let time = 20
 let isBirthday = false
+
+let greetingText = ''
+
+// let nameText
+// if (isLoggedIn && userName) {
+//   nameText = ', ' + userName
+// } else {
+//   nameText = ''
+// }
+
+let nameText = (isLoggedIn && userName) ? ', ' + userName : ''
+
+// let birthdayText
+// if (isBirthday && isLoggedIn) {
+//   birthdayText = ' and have a great birthday!'
+// } else {
+//   birthdayText = ''
+// }
+
+let birthdayText = (isBirthday && isLoggedIn) ? ' and have a great birthday!' : ''
+
+if (time >= 5 && time < 13) {
+  greetingText = 'Good Morning'
+} else if (time >= 13 && time < 19) {
+  greetingText = 'Good Afternoon'
+} else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+  greetingText = 'Good Evening'
+} else {
+  greetingText = 'Hello'
+}
+
+let greetingOutput = greetingText + nameText + birthdayText
+
+console.log(greetingOutput)
