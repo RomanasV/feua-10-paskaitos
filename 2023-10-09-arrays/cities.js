35fef265e8e8a193299743417df89b3aeae2b45a
RomanasV <romanasvenckus@gmail.com>
2023-10-16 20:01:57 +0300

    cities styling

--- 2023-10-09-arrays/cities.js
+++ 2023-10-09-arrays/cities.js
@@ -3,31 +3,101 @@ const cities = [
     name: 'Vilnius',
     population: 500000,
     location: {
-      continent: 'Europe',
-      country: 'Lithuania',
+        continent: 'Europe',
+        country: 'Lietuva',
     },
-    touristAttractions: ['Gedimino pilis', 'antra', 'trecia'],
+    touristAttractions: ['Gedimino pilies bokstas', 'Vilniaus katedra'],
     isCapital: true,
-  },
-  {
-    name: 'Riga',
-    population: 300000,
+},
+{
+    name: 'New York',
+    population: 8500000,
     location: {
-      continent: 'Europe',
-      country: 'Latvia',
+        continent: 'North America',
+        country: 'United States',
     },
-    touristAttractions: ['Gedimino pilis', 'antra', 'trecia'],
+    touristAttractions: [],
     isCapital: false,
-  },
-  {
-    name: 'Paris',
-    population: 5000000,
+},
+{
+    name: 'Tokyo',
+    population: 14000000,
     location: {
-      continent: 'Europe',
-      country: 'France',
+        continent: 'Asia',
+        country: 'Japan',
+    },
+    touristAttractions: ['Sensō-ji'],
+    isCapital: true,
+},
+{
+    name: 'Amsterdam',
+    population: 1400000,
+    location: {
+        continent: 'Europe',
+        country: 'Netherlands',
+    },
+    touristAttractions: ['Van gogh museum', 'Rijksmuseum', 'Anne Frank museum'],
+    isCapital: true,
+},
+{
+    name: 'Monaco',
+    population: 40000,
+    location: {
+        continent: 'Europe',
+        country: 'Monaco',
     },
     touristAttractions: [],
     isCapital: true,
+},
+{
+    name: 'Havana',
+    population: 2400000,
+    location: {
+        continent: 'North America',
+        country: 'Cuba',
+    },
+    touristAttractions: ['National Capitol of Cuba', 'Plaza de la Catedral'],
+    isCapital: true,
+},
+{
+    name: 'Singapore',
+    population: 5600000,
+    location: {
+        continent: 'Asia',
+        country: 'Singapore',
+    },
+    touristAttractions: ['Marina Bay Sands', 'Gardens by the Bay', 'Singapore Zoo'],
+    isCapital: true,
+},
+{
+    name: 'Melbourne',
+    population: 5000000,
+    location: {
+        continent: 'Australia',
+        country: 'Australia',
+    },
+    touristAttractions: ['Melbourne Skydeck'],
+    isCapital: false,
+  },
+  // {
+  //     name: 'Sapporo',
+  //     population: 1900000,
+  //     location: {
+  //         continent: 'Asia',
+  //         country: 'Japan',
+  //     },
+  //     touristAttractions: ['Hokkaido Jingu'],
+  //     isCapital: false,
+  // },
+  {
+      name: 'Miami',
+      population: 400000,
+      location: {
+          continent: 'North America',
+          country: 'United States',
+      },
+      touristAttractions: ['Miami Zoo', 'Ocean drive', 'Little Havana', 'Vizcaya Museum & Gardens'],
+      isCapital: false,
   },
 ]
 
@@ -76,10 +146,27 @@ renderCities()
 
 function styleCities() {
   const citiesCapitals = document.querySelectorAll('.capital h2')
-  
+
   citiesCapitals.forEach((capitalTitle) => {
     capitalTitle.style.color = 'red'
   })
+
+  // const oddCities = document.querySelectorAll('.city-wrapper:nth-of-type(odd)')
+  // oddCities.forEach(city => {
+  //   city.style.backgroundColor = '#f0f0f0'
+  // })
+
+  const allCities = document.querySelectorAll('.city-wrapper')
+
+  // allCities.forEach((city, index) => {
+  //   if (index % 2 === 0) {
+  //     city.style.backgroundColor = '#f0f0f0'
+  //   }
+  // })
+
+  for (let i = 0; i < allCities.length; i += 2) {
+    allCities[i].style.backgroundColor = '#f0f0f0'
+  }
 }
 
 styleCities()
