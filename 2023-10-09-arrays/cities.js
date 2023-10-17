a94a414b22397466a54e072a2cd236758767fd16
RomanasV <romanasvenckus@gmail.com>
2023-10-17 18:48:08 +0300

    cities task done

--- 2023-10-09-arrays/cities.js
+++ 2023-10-09-arrays/cities.js
@@ -68,8 +68,8 @@ const cities = [
     },
     touristAttractions: ['Marina Bay Sands', 'Gardens by the Bay', 'Singapore Zoo'],
     isCapital: true,
-},
-{
+  },
+  {
     name: 'Melbourne',
     population: 5000000,
     location: {
@@ -105,39 +105,52 @@ function renderCities() {
   const citiesList = document.querySelector('#cities-list')
 
   cities.forEach((city) => {
-    console.log(city)
-    console.log(city.name)
-    console.log(city.population)
-    console.log(city.isCapital)
-    console.log(city.location)
-    console.log(city.location.continent)
-    console.log(city.location.country)
-  
-    console.log(city.touristAttractions)
-    console.log(city.touristAttractions.length)
-  
-    city.touristAttractions.forEach(attraction => {
-      console.log(attraction)
-    })
-
     const cityWrapper = document.createElement('div')
     cityWrapper.classList.add('city-wrapper')
 
     let capitalTitle = ''
+    let capitalDescription = ''
 
     if (city.isCapital) {
       cityWrapper.classList.add('capital')
       capitalTitle = ' (capital)'
+      capitalDescription = ` ${city.name} is the capital of ${city.location.country}.`
     }
   
     const cityTitle = document.createElement('h2')
     cityTitle.textContent = city.name + capitalTitle
   
     const cityDescription = document.createElement('p')
-    cityDescription.textContent = `${city.name} city is located in ${city.location.continent}, ${city.location.country} and has population of ${city.population} people.`
+    cityDescription.textContent = `${city.name} city is located in ${city.location.continent}, ${city.location.country} and has population of ${city.population} people.${capitalDescription}`
   
     cityWrapper.append(cityTitle, cityDescription)
+
+    if (city.touristAttractions.length > 0) {
+      const touristAttractionsWrapper = document.createElement('div')
+      touristAttractionsWrapper.classList.add('tourist-attractions-list')
   
+      const touristAttractionsTitle = document.createElement('h3')
+  
+      if (city.touristAttractions.length > 1) {
+        touristAttractionsTitle.textContent = `Tourist attractions of ${city.name} are:`
+      } else {
+        touristAttractionsTitle.textContent = `Tourist attraction of ${city.name} is:`
+      }
+  
+      const touristAttractionsList = document.createElement('ul')
+  
+      city.touristAttractions.forEach(attraction => {
+        const touristAttractionItem = document.createElement('li')
+        touristAttractionItem.textContent = attraction
+  
+        touristAttractionsList.append(touristAttractionItem)
+      })
+  
+      touristAttractionsWrapper.append(touristAttractionsTitle, touristAttractionsList)
+  
+      cityWrapper.append(touristAttractionsWrapper)
+    }
+
     citiesList.append(cityWrapper)
   })
 }
@@ -167,6 +180,51 @@ function styleCities() {
   for (let i = 0; i < allCities.length; i += 2) {
     allCities[i].style.backgroundColor = '#f0f0f0'
   }
+
+  // const firstAttractionElements = document.querySelectorAll('li:first-of-type')
+  // firstAttractionElements.forEach(element => {
+  //   element.style.color = 'green'
+  // })
+
+  // const lastAttractionElements = document.querySelectorAll('li:nth-of-type(1n + 4)')
+  // lastAttractionElements.forEach(element => {
+  //   element.style.color = 'red'
+  // })
+
+  const touristAttractionLists = document.querySelectorAll('.tourist-attractions-list ul')
+  touristAttractionLists.forEach(ulElement => {
+    const liElements = ulElement.querySelectorAll('li')
+    liElements.forEach((liElement, index) => {
+      if (index === 0) {
+        liElement.style.color = 'green'
+      } else if (index > 2) {
+        liElement.style.color = 'red'
+      } else {
+        liElement.style.color = 'orange'
+      }
+    })
+  })
+
+  const citiesList = document.querySelector('#cities-list')
+  // citiesList.style.display = 'flex'
+  // citiesList.style.flexWrap = 'wrap'
+  // citiesList.style.gap = '20px'
+
+  citiesList.style = `display: flex;
+                      flex-wrap: wrap;
+                      gap: 20px;`
+
+  allCities.forEach((city, index) => {
+    console.log(index, city)
+    city.style.padding = '15px'
+    city.style.boxSizing = 'border-box'
+    
+    if (index === allCities.length - 1 && index % 2 === 0) {
+      city.style.width = '100%'
+    } else {
+      city.style.width = 'calc((100% - 20px) / 2)'
+    }
+  })
 }
 
 styleCities()
